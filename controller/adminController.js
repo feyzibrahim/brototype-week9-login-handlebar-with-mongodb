4f612b8750954924550ec953e2aef782ca229421
Feyz Ibrahim <faizashrafkk@gmail.com>
2023-09-14 00:41:22 +0530

    admin panel setted up

--- controller/adminController.js
+++ controller/adminController.js
@@ -1,4 +1,7 @@
 const { getDb } = require("../db/db");
+const { ObjectId } = require("mongodb");
+const bcrypt = require("bcrypt");
+const validator = require("validator");
 
 const getUsersList = async (req, res) => {
   const db = getDb();
@@ -19,4 +22,120 @@ const getUsersList = async (req, res) => {
     });
 };
 
-module.exports = { getUsersList };
+const deleteUser = (req, res) => {
+  const { id } = req.params;
+  const db = getDb();
+  db.collection("user")
+    .deleteOne({ _id: new ObjectId(id) })
+    .then(() => {
+      res.redirect("/");
+    })
+    .catch((err) => {
+      console.log(err);
+      res.redirect("/");
+    });
+};
+
+const editUser = async (req, res) => {
+  const { id } = req.params;
+  const db = getDb();
+  const user = await db.collection("user").findOne({ _id: new ObjectId(id) });
+
+  if (user) {
+    res.render("admin/edit", { user: user, isLoggedIn: true, err: "" });
+  } else {
+    res.redirect("/");
+  }
+};
+
+const updateUser = async (req, res) => {
+  const { id } = req.params;
+  const db = getDb();
+  const body = req.body;
+
+  if (body.password !== "") {
+    const salt = 10;
+    const hash = await bcrypt.hash(body.password, salt);
+
+    body.password = hash;
+  } else {
+    delete body.password;
+  }
+
+  const update = await db.collection("user").updateOne(
+    { _id: new ObjectId(id) },
+    {
+      $set: body,
+    }
+  );
+  console.log(update);
+  res.redirect("/");
+};
+
+const newUser = (req, res) => {
+  res.render("admin/newUser");
+};
+
+const createNewUser = async (req, res) => {
+  try {
+    const { name, email, password, passwordA } = req.body;
+
+    // Form Validation
+    if (!name || !email || !password || !passwordA) {
+      throw "All fields are required...";
+    }
+
+    if (!validator.isEmail(email)) {
+      throw "Email is Not Valid...";
+    }
+
+    if (password !== passwordA) {
+      throw "Password doesn't Match...";
+    }
+
+    if (!validator.isStrongPassword(password)) {
+      throw "Password is Not Strong";
+    }
+
+    const db = getDb();
+
+    // Checking for duplicate email in db
+    const exists = await db.collection("user").findOne({ email });
+
+    if (exists) {
+      throw "Email Already Used...";
+    }
+
+    // Encryption
+    const salt = 10;
+    const hash = await bcrypt.hash(password, salt);
+
+    // Saving to DB
+    db.collection("user")
+      .insertOne({
+        name: name,
+        email: email,
+        password: hash,
+        date: Date.now(),
+        roll: "user",
+      })
+      .then(() => {
+        // Redirecting to home
+        res.redirect("/");
+      })
+      .catch((error) => {
+        res.render("admin/newUser", { err: error });
+      });
+  } catch (error) {
+    res.render("admin/newUser", { err: error });
+  }
+};
+
+module.exports = {
+  getUsersList,
+  deleteUser,
+  editUser,
+  updateUser,
+  newUser,
+  createNewUser,
+};
--- routes/admin.js
+++ routes/admin.js
@@ -1,11 +1,28 @@
 const express = require("express");
 const { verifyAuth } = require("../middleware/auth");
-const { getUsersList } = require("../controller/adminController");
+const {
+  getUsersList,
+  deleteUser,
+  editUser,
+  updateUser,
+  newUser,
+  createNewUser,
+} = require("../controller/adminController");
 
 const router = express.Router();
 
 router.use(verifyAuth);
 
 router.get("/", getUsersList);
+
+router.get("/delete/:id", deleteUser);
+
+router.get("/edit/:id", editUser);
+
+router.post("/edit/:id", updateUser);
+
+router.get("/newUser", newUser);
+
+router.post("/newUser", createNewUser);
 
 module.exports = router;
